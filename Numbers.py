1b21bbfc7c0eedec7c84c0947752a6b4ede6316f
NathanScott85 <nathansfeely@gmail.com>
2018-07-13 19:38:43 +0100

    Added NumbersPY

---
+++ Numbers.py
@@ -0,0 +1,19 @@
+Pi = 3.14
+number_one = 3
+number_two = 5
+number_three = 7
+number_four = 8
+negative_Five = -5
+
+#Converting numbers to strings
+print(str(Pi)+ " This is a string")
+#multiple Pi by 2
+print(Pi * 2)
+print(number_four - number_one)
+print(number_four + negative_Five)
+#Using order of operation
+print(number_four * (number_three + number_four))
+print(number_four * (Pi / number_three))
+
+#Modular giving the remainder
+print (number_four % number_one)
