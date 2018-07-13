06f48abf523007fa9a9e6fc7ee8ca33cc8b0452c
NathanScott85 <nathansfeely@gmail.com>
2018-07-13 12:54:19 +0100

    first commit

---
+++ App.py
@@ -0,0 +1,8 @@
+character_name = "John"
+character_age = "35"
+is_Male = True
+print("There once was a man named "+ character_name + ",")
+print("he was " + character_age + " years old")
+character_name = "Mike"
+print("He really liked the name " +character_name +",")
+print("but didnt like been " + character_age)
