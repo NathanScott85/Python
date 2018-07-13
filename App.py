3eb53501f3c8d46cb5f1397722ca4a5ecfbb09a6
NathanScott85 <nathansfeely@gmail.com>
2018-07-13 13:52:08 +0100

    Modify Values, lower and isLower & letter in a string

--- App.py
+++ App.py
@@ -2,25 +2,33 @@
 from Test import *
 from Test import John
 
+#This is how we do comments
 #Assigning String Values
 character_name = "John"
 character_age = "35"
 #True or false Booleans
-
 is_Male = True
 
 #Passing Values to print
 print("There once was a man named "+ character_name + ",")
 print("he was " + character_age + " years old")
+#Modifying Values
 character_name = "Mike"
 print("He really liked the name " +character_name +",")
-print("but didnt like been " + character_age)
+character_age = "105"
+print("but does not like been age " + character_age)
 
-#This is how we do comments
 # \n is a new line
+#Converting to lower case and checking if a string is lower case
 phrase = "Python\n Academy"
-print(phrase)
+print(phrase.lower().islower())
+#Length of a string
+
+#Grab a letter from a word in a string
+#Can also assign .upper after getting the letter from a string to change its casing
+print(phrase[3].upper())
+print(phrase, len(phrase))
 #String concatenation
 phrase_two = "Python "
 phrase_three = "Learning"
-print(phrase_two + phrase_three) 
+print(phrase_two.lower() + phrase_three.upper()) 
