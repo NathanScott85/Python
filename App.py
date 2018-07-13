87025ec4c85efd68c4ebc952a65475564f5c2ddf
NathanScott85 <nathansfeely@gmail.com>
2018-07-13 13:16:49 +0100

    True/False/Concatenation\NewLines\Imports

--- App.py
+++ App.py
@@ -1,8 +1,26 @@
+#Import Statements
+from Test import *
+from Test import John
+
+#Assigning String Values
 character_name = "John"
 character_age = "35"
+#True or false Booleans
+
 is_Male = True
+
+#Passing Values to print
 print("There once was a man named "+ character_name + ",")
 print("he was " + character_age + " years old")
 character_name = "Mike"
 print("He really liked the name " +character_name +",")
 print("but didnt like been " + character_age)
+
+#This is how we do comments
+# \n is a new line
+phrase = "Python\n Academy"
+print(phrase)
+#String concatenation
+phrase_two = "Python "
+phrase_three = "Learning"
+print(phrase_two + phrase_three) 
